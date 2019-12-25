fa8d3de71e738de144577a6150cf9e99f4394efa
duxk@wstianxia.com <duxk@wstianxia.com>
2019-12-25 15:19:07 +0800

    云开发提交

---
+++ public/static/config.js
@@ -0,0 +1,16 @@
+﻿/**
+ * vue打包后保留的配置文件
+ */
+
+module.exports = {
+  // url: 'https://crm.netbcloud.com'
+  // url: 'https://crmnn.netbcloud.com'
+  url: './'
+  // url: 'https://service-owpjn44i-1259193132.gz.apigw.tencentcs.com/release'
+  // url: 'https://sgcrm.wegouer.com'
+  // url: 'https://crmweb.netbcloud.com'
+  // url: 'https://duliban-test.netbcloud.com'
+  // url: 'https://qscrm.xinhankr.com' // 青商
+  // url: 'https://jdb.wegouer.com' // 街道办测试
+  // url: 'https://dev-hgh.wegouer.com/release/crm-api' // 云开发测试
+}
